265199f8d2fec46400ecad510b434c9faa3ab7a5
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-08 12:16:03 +0530

    MODIFIED: function for Number 1,10,100,1000 and display unit, ten hundred

--- src/main/java/org/example/operators/ifElsestmt.java
+++ src/main/java/org/example/operators/ifElsestmt.java
@@ -6,6 +6,39 @@ public class ifElsestmt {
     public static void main(String[] args) {
         ifElsestmt ifelse = new ifElsestmt();
         ifelse.readDigitandWriteinWord();
+
+        ifelse.displayUnit();
+    }
+
+    private void displayUnit() {
+        Scanner input = new Scanner(System.in);
+        int thousand=0;
+        int hundreds=0;
+        int tens=0;
+        int ones=0;
+
+        //input 4 digit number
+
+        System.out.println("Enter 4 digit number:");
+        int number = input.nextInt();
+        if(number <= 9999 && number >999){
+            thousand = number/1000;
+            System.out.println("thousands place digit:" + thousand);
+
+            hundreds = number/100;
+            System.out.println("hundreds place digit:" + hundreds);
+
+            tens = number/10;
+            System.out.println("tens place digit:" + tens);
+
+            ones = number/1;
+            System.out.println("ones place digit:" + ones);
+        } else {
+            if(number > 9999)
+            System.out.println("\n Error! Number more  than digits ");
+            if(number < 1000)
+                System.out.println("\n Error! Number less  than 4 digits ");
+        }
     }
 
     private void readDigitandWriteinWord() {
