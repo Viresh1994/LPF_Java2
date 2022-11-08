dc1e0b3d63c67a05b9efc51099fe2fe799e2c4de
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-08 13:14:13 +0530

    MODIFIED: Function for reverse no

--- src/main/java/org/example/operators/naturalNo.java
+++ src/main/java/org/example/operators/naturalNo.java
@@ -6,6 +6,18 @@ public class naturalNo {
         int n = 5;
         System.out.println(natural.findSum(n));
 
+        natural.reverseNo();
+
+    }
+
+    private void reverseNo() {
+        int num = 987654, rev=0;
+        while(num != 0){
+            int rem = num % 10;
+            rev = rev * 10 + rem;
+            num = num/10;
+        }
+        System.out.println(" The reverse of the given numbers is: " + rev);
     }
 
     private int findSum(int n) {
