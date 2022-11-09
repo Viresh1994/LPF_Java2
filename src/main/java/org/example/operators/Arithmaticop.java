130811e815548485173695d7675e99e85040d2ec
Viresh1994 <vireshanadakatni@gmail.com>
2022-11-09 23:52:05 +0530

    MODIFIED: code for min function

--- src/main/java/org/example/operators/Arithmaticop.java
+++ src/main/java/org/example/operators/Arithmaticop.java
@@ -19,36 +19,67 @@ public class Arithmaticop {
                 res2 = c + a / b;
                 res3 = a % b + c;
                 res4 = a * b + c;
+                Arithmaticop aop = new Arithmaticop();
+                aop.max(res1, res2, res3, res4);
 
-                if(res1 > res2)
-                {
-                        if(res1>res3)
-                        {
-                                if(res1>res4)
-                                        System.out.println("Max is res1: " + res1);
-                                 else
-                                        System.out.println("Max is res4: " + res4);
+                aop.min(res1, res2, res3, res4);
+        }
+
+        private void min(int res1, int res2, int res3, int res4) {
+                if (res1 < res2) {
+                        if (res1 < res3) {
+                                if (res1 < res4)
+                                        System.out.println("Min is res1: " + res1);
+                                else
+                                        System.out.println("Min is res4: " + res4);
+                        } else {
+                                if (res3 < res4)
+                                        System.out.println("Min is res3: " + res3);
+                                else
+                                        System.out.println("Min is res4: " + res4);
                         }
-                        else{
-                                if(res3 > res4)
+                } else {
+                        if (res2 < res3) {
+                                if (res2 < res4)
+                                        System.out.println("Min is res2: " + res2);
+                                else
+                                        System.out.println("Min is res4: " + res4);
+                        } else {
+                                if (res3 < res4)
+                                        System.out.println("Min is res3: " + res3);
+                                else
+                                        System.out.println("Min is res4: " + res4);
+                        }
+                }
+        }
+
+        public void max(int res1, int res2, int res3, int res4) {
+                if (res1 > res2) {
+                        if (res1 > res3) {
+                                if (res1 > res4)
+                                        System.out.println("Max is res1: " + res1);
+                                else
+                                        System.out.println("Max is res4: " + res4);
+                        } else {
+                                if (res3 > res4)
                                         System.out.println("Max is res3: " + res3);
                                 else
                                         System.out.println("Max is res4: " + res4);
                         }
-                }
-                else {
-                        if(res2>res3){
-                                if(res2>res4)
+                } else {
+                        if (res2 > res3) {
+                                if (res2 > res4)
                                         System.out.println("Max is res2: " + res2);
                                 else
                                         System.out.println("Max is res4: " + res4);
-                        }
-                        else{
-                                if(res3 > res4)
+                        } else {
+                                if (res3 > res4)
                                         System.out.println("Max is res3: " + res3);
                                 else
                                         System.out.println("Max is res4: " + res4);
                         }
                 }
-    }
+        }
 }
+
+
